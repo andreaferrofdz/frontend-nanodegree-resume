9f1a5c6664d5d2d392183a8c75022482427a01ae
Andrea Ferro <andreaferrofdz@gmail.com>
2016-06-21 09:47:09 -0500

    Code review corrections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,92 +2,92 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
+ var data = "%data%";
+
 var bio = {
 	"name": "Andrea Ferro",
 	"role": "Web Developer",
-	"contactInfo": {
+	"contacts": {
 		"mobile": "5512954423",
 		"email": "andreaferrofdz@gmail.com",	
 		"github": "andreaferrofdz",
 		"location": "Mexico City"
 	},
-	"picture": "images/andrea.jpg",
-	"welcomeMsg": "The time to be awesome is now!!!",
+	"biopic": "images/andrea.jpg",
+	"welcomeMessage": "The time to be awesome is now!!!",
 	"skills": ["html","css","asp.net", "sql","oracle", "c#", "vb"]
 };
 
 bio.display = function() {
 
-	var formattedName = HTMLheaderName.replace("%data%", "Andrea Ferro");
-	var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+	var formattedName = HTMLheaderName.replace(data, "Andrea Ferro");
+	var formattedRole = HTMLheaderRole.replace(data, "Web Developer");
 
 	$("#header").prepend(formattedRole);
 	$("#header").prepend(formattedName);
 
-	$("#topContacts").prepend(HTMLlocation.replace("%data%", bio.contactInfo.location));
-	$("#topContacts").prepend(HTMLgithub.replace("%data%", bio.contactInfo.github));
-	$("#topContacts").prepend(HTMLemail.replace("%data%", bio.contactInfo.email));
-	$("#topContacts").prepend(HTMLmobile.replace("%data%", bio.contactInfo.mobile));
+	$("#topContacts").prepend(HTMLlocation.replace(data, bio.contacts.location));
+	$("#topContacts").prepend(HTMLgithub.replace(data, bio.contacts.github));
+	$("#topContacts").prepend(HTMLemail.replace(data, bio.contacts.email));
+	$("#topContacts").prepend(HTMLmobile.replace(data, bio.contacts.mobile));
 
-	$("#footerContacts").prepend(HTMLlocation.replace("%data%", bio.contactInfo.location));
-	$("#footerContacts").prepend(HTMLgithub.replace("%data%", bio.contactInfo.github));
-	$("#footerContacts").prepend(HTMLemail.replace("%data%", bio.contactInfo.email));
-	$("#footerContacts").prepend(HTMLmobile.replace("%data%", bio.contactInfo.mobile));
+	$("#footerContacts").prepend(HTMLlocation.replace(data, bio.contacts.location));
+	$("#footerContacts").prepend(HTMLgithub.replace(data, bio.contacts.github));
+	$("#footerContacts").prepend(HTMLemail.replace(data, bio.contacts.email));
+	$("#footerContacts").prepend(HTMLmobile.replace(data, bio.contacts.mobile));
 
-	$("#header").append(HTMLbioPic.replace("%data%", bio.picture));
-	$("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg));
+	$("#header").append(HTMLbioPic.replace(data, bio.biopic));
+	$("#header").append(HTMLwelcomeMsg.replace(data, bio.welcomeMessage));
 
 	$("#header").append(HTMLskillsStart);
 
 
 	for (var skill in bio.skills) {
 		if(bio.skills.hasOwnProperty(skill)) {
-	  		$("#skills").append(HTMLskills.replace("%data%", bio.skills[skill]));
+	  		$("#skills").append(HTMLskills.replace(data, bio.skills[skill]));
 	  	}
 	}
 
 };
 
 var work = {
-	"jobs": [
-		{
+	"jobs": [{
 			"employer": "Me",
 			"title": "Freelacer",
 			"dates": "2016 - Future",
-			"description": "Development and supporting web applications."
+			"description": "Development and supporting web applications.",
+			"location": "Mexico City"
 		},
 		{
 			"employer": "TNS",
 			"title": "Web Developer",
 			"dates": "2015 - 2016",
-			"description": "Development and supporting web applications. Creating database tables, queries and scripts on multiple DBMS (SQL Server, MYSQL). Management of change control for multiple environments (DEV, UAT and PROD). Organization and monitoring of SIT and UAT testings."
+			"description": "Development and supporting web applications. Creating database tables, queries and scripts on multiple DBMS (SQL Server, MYSQL). Management of change control for multiple environments (DEV, UAT and PROD). Organization and monitoring of SIT and UAT testings.",
+			"location": "Mexico City"
 		},
 		{
 			"employer": "JP Morgan Chase",
 			"title": "Trainee, Application Delivery Mexico",
 			"dates": "2013 - 2015",
-			"description": "Development and supporting BI processes using Pentaho tool for regulatory reporting of finance, operations and accounting. Development and support of macros for reporting purposes. Help with the support in front and middle office on the trading floor. Creating database tables, queries and scripts on multiple DBMS (Oracle, Sybase, SQL Server). Management of change control for multiple environments (DEV, UAT and PROD). Organization and monitoring of SIT and UAT testings. Solution and control of incidents in production. Developed an application to have monitored all incidents affecting different areas of business including systems"
-		}
-	]
-
-
+			"description": "Development and supporting BI processes using Pentaho tool for regulatory reporting of finance, operations and accounting. Development and support of macros for reporting purposes. Help with the support in front and middle office on the trading floor. Creating database tables, queries and scripts on multiple DBMS (Oracle, Sybase, SQL Server). Management of change control for multiple environments (DEV, UAT and PROD). Organization and monitoring of SIT and UAT testings. Solution and control of incidents in production. Developed an application to have monitored all incidents affecting different areas of business including systems",
+			"location": "Mexico City"
+		}]
 };
 
 work.display = function () {
 
-	for (var job in work.jobs) {
-		if(work.jobs.hasOwnProperty(job)) {
-		 	$("#workExperience").append(HTMLworkStart);
+	for (var indexCount = 0; indexCount < work.jobs.length; indexCount++) {
+		$("#workExperience").append(HTMLworkStart);
 
-		 	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-		 	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-		 	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		var formattedEmployer = HTMLworkEmployer.replace(data, work.jobs[indexCount].employer);
+		var formattedTitle = HTMLworkTitle.replace(data, work.jobs[indexCount].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
 
-		 	$(".work-entry:last").append(formattedEmployerTitle);
+		$(".work-entry:last").append(formattedEmployerTitle);
 
-		 	$(".work-entry:last").append(HTMLworkDates.replace("%data%", work.jobs[job].dates));
-			$(".work-entry:last").append(HTMLworkDescription.replace("%data%", work.jobs[job].description));
-		}
+		$(".work-entry:last").append(HTMLworkDates.replace(data, work.jobs[indexCount].dates));
+		$(".work-entry:last").append(HTMLworkDescription.replace(data, work.jobs[indexCount].description));
+		
 	}
 
 };
@@ -99,14 +99,15 @@ var education = {
 			"dates": "2010-2014",
 			"location": "Mexico City",
 			"degree": "Bachelor",
-			"major": "Engineering Cybernetics and Computers Systems"
+			"majors": ["Engineering Cybernetics and Computers Systems"],
+			"url": ["http://www.lasalle.mx/"]
 		}
 	],
 	"onlineCourses": [
 		{
 			"title": "Front-End Web Developer Nanodegree",
 			"school": "Udacity",
-			"dates": "2016",
+			"date": "2016",
 			"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
 		}
 	]
@@ -117,10 +118,13 @@ education.display = function () {
 		if(education.schools.hasOwnProperty(school)) {
 			$("#education").append(HTMLschoolStart);
 
-			$(".education-entry:last").append(HTMLschoolName.replace("%data%", education.schools[school].name) + HTMLschoolDegree.replace("%data%", education.schools[school].degree));
-			$(".education-entry:last").append(HTMLschoolDates.replace("%data%", education.schools[school].dates));
-			$(".education-entry:last").append(HTMLschoolLocation.replace("%data%", education.schools[school].location));
-			$(".education-entry:last").append(HTMLschoolMajor.replace("%data%", education.schools[school].major));
+			$(".education-entry:last").append(HTMLschoolName.replace(data, education.schools[school].name) + HTMLschoolDegree.replace(data, education.schools[school].degree));
+			$(".education-entry:last").append(HTMLschoolDates.replace(data, education.schools[school].date));
+			$(".education-entry:last").append(HTMLschoolLocation.replace(data, education.schools[school].location));
+
+			for (var indexCount = 0; indexCount < education.schools[school].majors.length; indexCount++) {
+				$(".education-entry:last").append(HTMLschoolMajor.replace(data, education.schools[school].majors[indexCount]));
+			}
 		}
 	}
 
@@ -128,9 +132,9 @@ education.display = function () {
 		if(education.onlineCourses.hasOwnProperty(onlineCourse)) {
 			$(".education-entry:last").append(HTMLonlineClasses);
 
-			$(".education-entry:last").append(HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse].title) + HTMLonlineSchool.replace("%data%", education.onlineCourses[onlineCourse].school));
-			$(".education-entry:last").append(HTMLonlineDates.replace("%data%", education.onlineCourses[onlineCourse].dates));
-			$(".education-entry:last").append(HTMLonlineURL.replace("%data%", education.onlineCourses[onlineCourse].url));
+			$(".education-entry:last").append(HTMLonlineTitle.replace(data, education.onlineCourses[onlineCourse].title) + HTMLonlineSchool.replace(data, education.onlineCourses[onlineCourse].school));
+			$(".education-entry:last").append(HTMLonlineDates.replace(data, education.onlineCourses[onlineCourse].dates));
+			$(".education-entry:last").append(HTMLonlineURL.replace(data, education.onlineCourses[onlineCourse].url));
 		}
 	}
 
@@ -154,13 +158,13 @@ projects.display = function () {
 		if(projects.projects.hasOwnProperty(project)) {
 			$("#projects").append(HTMLprojectStart);
 
-			$(".project-entry:last").append(HTMLprojectTitle.replace("%data%", projects.projects[project].title));
-			$(".project-entry:last").append(HTMLprojectDates.replace("%data%", projects.projects[project].dates));
-			$(".project-entry:last").append(HTMLprojectDescription.replace("%data%", projects.projects[project].description));
+			$(".project-entry:last").append(HTMLprojectTitle.replace(data, projects.projects[project].title));
+			$(".project-entry:last").append(HTMLprojectDates.replace(data, projects.projects[project].dates));
+			$(".project-entry:last").append(HTMLprojectDescription.replace(data, projects.projects[project].description));
 
 			for (var image in projects.projects[project].images) {
 				if(projects.projects[project].images.hasOwnProperty(image)) {
-	  				$(".project-entry:last").append(HTMLprojectImage.replace("%data%", projects.projects[project].images[image]));
+	  				$(".project-entry:last").append(HTMLprojectImage.replace(data, projects.projects[project].images[image]));
 	  			}
 			}
 		}
